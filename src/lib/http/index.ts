e3f7b7b9ad7ae7758da1ffbfff859be8729df073
billyparmenter <bparmenter6387@conestogac.on.ca>
2023-04-08 13:41:23 -0400

    hard coded url

--- src/lib/http/index.ts
+++ src/lib/http/index.ts
@@ -2,7 +2,7 @@ import { apiUrl } from "../../util/url";
 import { getError, makeAxiosApi } from "./axios";
 import { makeHttp } from "./http";
 
-const api = makeAxiosApi(apiUrl);
+const api = makeAxiosApi("https://dev.smartjournal.net:443/um/test/api/jr/txn");
 
 export const http = makeHttp(api, getError);
 
